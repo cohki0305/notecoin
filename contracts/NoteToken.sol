fddd53b4283acc288a9fc6f0a5162948d5e48b68
cohki0305 <cohki0305@gmail.com>
2018-04-14 09:52:57 +0900

    compile token

---
+++ contracts/NoteToken.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.4.17;
+
+import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+
+contract NoteToken is StandardToken {
+  string public name           = 'NoteToken';
+  string public symbol         = 'NT';
+  uint8  public demicals       = 2;
+  uint   public INITIAL_SUPPLY = 12000;
+
+  function NoteToken() public {
+    totalSupply_ = INITIAL_SUPPLY;
+    balances[msg.sender] = INITIAL_SUPPLY;
+  }
+}
